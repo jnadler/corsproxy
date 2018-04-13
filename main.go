bbf1d6f5a60089bc9cfd37e21fa8a1d5f36b3166
Jeff Nadler <jnadler@digitalocean.com>
2018-04-13 13:59:06 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+var ListenPort = "9200"
+var RemoteUrl = "http://elasticsearch.internal.digitalocean.com:9200/"
+
+// Add CORS Headers
+func addCORSHeaders(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		handler.ServeHTTP(w, r)
+	})
+}
+
+// Replace the hostname
+func setDestinationHost(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Host = r.URL.Host
+		handler.ServeHTTP(w, r)
+	})
+}
+
+// Run a SingleHostReverseProxy to RemoteUrl on local port ListenPort
+func Proxy(remoteUrl string) http.Handler {
+	serverUrl, err := url.Parse(remoteUrl)
+	if err != nil {
+		log.Fatal("URL failed to parse")
+	}
+
+	reverseProxy := httputil.NewSingleHostReverseProxy(serverUrl)
+	singleHosted := setDestinationHost(reverseProxy)
+	return addCORSHeaders(singleHosted)
+}
+
+func main() {
+	println("Starting proxy for " + RemoteUrl + " on local port " + ListenPort)
+	proxy := Proxy(RemoteUrl)
+	http.ListenAndServe(":"+ListenPort, proxy)
+}
